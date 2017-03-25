5a122216942b032fcee773c3796c3b312ac6c9a9
Lautaro <lautydb@yahoo.com>
2017-03-25 12:21:06 -0300

    bloque

--- PP2/src/dto/Bloque.java
+++ PP2/src/dto/Bloque.java
@@ -3,10 +3,43 @@ package dto;
 public class Bloque {
 	double x = 0;
 	double y = 0;
-	double ancho = 10;
-	double largo = 10;
+	double ancho = 20;
+	double largo = 20;
 	
 	public Bloque (double x, double y){
-		
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getAncho() {
+		return ancho;
+	}
+
+	public void setAncho(double ancho) {
+		this.ancho = ancho;
+	}
+
+	public double getLargo() {
+		return largo;
+	}
+
+	public void setLargo(double largo) {
+		this.largo = largo;
 	}
 }
