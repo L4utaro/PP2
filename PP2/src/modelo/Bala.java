d257cab1309d8d6624ca0f4ca8160f7ca22131ce
Lautaro <lautydb@yahoo.com>
2017-03-26 17:46:22 -0300

    asfd

--- PP2/src/modelo/Bala.java
+++ PP2/src/modelo/Bala.java
@@ -1,9 +1,27 @@
 package modelo;
 
+import util.Util;
 public class Bala {
-
-	//orientacion
+	private Util util;
 	//movimiento
 	
-	//controlarPosicion
+	public Bala(){
+		util = new Util();
+	}
+	
+	public void disparar(){
+		//controlaLaOrientacionEnLa que se encuentra
+		//esa orientacion se la pasa a ControlarLimiteDeTablero, que controla si se puede disparar en esa direccion
+		//despues sale el disparo
+		controlarOrientacion();
+		util.controlarLimiteDeTablero();
+	}
+	
+	public String controlarOrientacion(){
+		return null;
+	}
+	
+	public void movimiento(){
+		
+	}
 }
