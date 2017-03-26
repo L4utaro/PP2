9ae07aaa79d9221c33349b29daaf7a7c9b6a852c
Lautaro <lautydb@yahoo.com>
2017-03-26 20:01:02 -0300

    baka

--- PP2/src/modelo/Bala.java
+++ PP2/src/modelo/Bala.java
@@ -1,5 +1,27 @@
 package modelo;
 
 public class Bala {
+	private double x;
+	private double y;
+	
+	public Bala (double x2, double y2){
+		this.x = x2;
+		this.y = y2;
+	}
 
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
 }
--- PP2/src/modelo/Disparo.java
+++ PP2/src/modelo/Disparo.java
@@ -10,8 +10,10 @@ public class Disparo {
 	private Util util;
 	private double anguloOrientacion = 0; // 0º, 90º, 180º ó 270º
 	private Bala bala;
+	private Tanque tanque;
 	
-	public Disparo(){
+	public Disparo(Tanque tanque){
+		this.tanque = tanque;
 		util = new Util();
 	}
 
@@ -20,15 +22,23 @@ public class Disparo {
 	//despues sale el disparo
 	public void disparar(Entorno ent){
 		if(util.controlarLimiteDeTablero(anguloOrientacion)){
-			bala = new Bala();
-			//ent.dibujarCirculo(TanqueX+1, tanqueY+1, 5, Color.red);
+			bala = new Bala(tanque.x,tanque.y);
 			movimiento(ent);
 		}
 	}
 	
 	public void movimiento(Entorno ent){
+		//mover circulo CONTROLANDO QUE NO CHOQUE CON EL FINAL
+		ent.dibujarCirculo(bala.getX()+1, bala.getY()+1, 5, Color.red);
 		if(anguloOrientacion == 0){
-			
+			this.bala.setY(this.bala.getY()+1);
+		}else if(anguloOrientacion == 90){
+			this.bala.setX(this.bala.getX()+1);
+		}else if(anguloOrientacion == 180){
+			this.bala.setY(this.bala.getY()-1);
+		}else if(anguloOrientacion == 270){
+			this.bala.setX(this.bala.getX()-1);
 		}
+		ent.dibujarCirculo(tanque.x+1, tanque.y+1, 5, Color.red);
 	}
 }
