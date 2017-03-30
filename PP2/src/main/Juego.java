0037807e9bb581c4c19ae21b0859dffb32ba5316
Lautaro <lautydb@yahoo.com>
2017-03-30 15:41:17 -0300

    fondo

--- PP2/src/main/Juego.java
+++ PP2/src/main/Juego.java
@@ -1,8 +1,10 @@
 package main;
 
 import java.awt.Color;
+import java.awt.Image;
 
 import entorno.Entorno;
+import entorno.Herramientas;
 import entorno.InterfaceJuego;
 import modelo.Tanque;
 
@@ -11,6 +13,7 @@ public class Juego extends InterfaceJuego {
     private Tanque tanque;
     private int ANCHO_ENTORNO = 800;
     private int ALTO_ENTORNO = 600;
+	private Image fondo = Herramientas.cargarImagen("imagen/fondo.jpg");
 	
 	public Juego() {
     	this.entorno = new Entorno(this, "Battle-City", ANCHO_ENTORNO, ALTO_ENTORNO);
@@ -21,11 +24,12 @@ public class Juego extends InterfaceJuego {
     public void tick() {
     	if (!esElFin()) {
         	this.entorno.dibujarRectangulo(300,300, 520, 520, 0, Color.gray);
-        	this.entorno.dibujarRectangulo(300,300, 500, 500, 0, Color.black);
+        	//this.entorno.dibujarRectangulo(300,300, 500, 500, 0, Color.black);
+        	this.entorno.dibujarImagen(fondo, 300, 300, 0);
         	
     		manejo_tanque();
     		
-    		manejo_disparo();
+    		//manejo_disparo();
     		
     	}
     	else{
@@ -39,36 +43,33 @@ public class Juego extends InterfaceJuego {
 
 	private void manejo_tanque() {
 		
-//		if(this.entorno.estaPresionada(this.entorno.TECLA_DERECHA) && this.tanque.getAnguloOrientacion() == 0){
-//			this.tanque.avanzarDerecha();
-//		}
-//		if(this.entorno.estaPresionada(this.entorno.TECLA_DERECHA) && this.tanque.getAnguloOrientacion()!=0){
-//			this.tanque.girar_Derecha();
-//		}
-//		
-//		//ARRIBA
-//		if(this.entorno.estaPresionada(this.entorno.TECLA_ARRIBA) && this.tanque.getAnguloOrientacion()==Math.PI*1.5){
-//			this.tanque.avanzarArriba();
-//		}
-//		if(this.entorno.estaPresionada(this.entorno.TECLA_ARRIBA) && this.tanque.getAnguloOrientacion()!=Math.PI*1.5){
-//			this.tanque.girar_Arriba();
-//		}
-//		
-//		//IZQUIERDA
-//		if(this.entorno.estaPresionada(this.entorno.TECLA_IZQUIERDA) && this.tanque.getAnguloOrientacion()==Math.PI){
-//			this.tanque.avanzarIzquierda();
-//		}
-//		if(this.entorno.estaPresionada(this.entorno.TECLA_IZQUIERDA) && this.tanque.getAnguloOrientacion()!=Math.PI){
-//			this.tanque.girar_Izquierda();
-//		}
-//		
-//		//ABAJO
-//		if(this.entorno.estaPresionada(this.entorno.TECLA_ABAJO) && this.tanque.getAnguloOrientacion()==Math.PI/2){
-//			this.tanque.avanzarAbajo();
-//		}
-//		if(this.entorno.estaPresionada(this.entorno.TECLA_ABAJO) && this.tanque.getAnguloOrientacion()!=Math.PI/2){
-//			this.tanque.girar_Abajo();
-//		}
+		if(this.entorno.estaPresionada(this.entorno.TECLA_DERECHA) && this.tanque.getAnguloOrientacion() == 0){
+			this.tanque.avanzarDerecha();
+		}
+		if(this.entorno.estaPresionada(this.entorno.TECLA_DERECHA) && this.tanque.getAnguloOrientacion()!=0){
+			this.tanque.girar_Derecha();
+		}
+		//ARRIBA
+		if(this.entorno.estaPresionada(this.entorno.TECLA_ARRIBA) && this.tanque.getAnguloOrientacion()==Math.PI*1.5){
+			this.tanque.avanzarArriba();
+		}
+		if(this.entorno.estaPresionada(this.entorno.TECLA_ARRIBA) && this.tanque.getAnguloOrientacion()!=Math.PI*1.5){
+			this.tanque.girar_Arriba();
+		}
+		//IZQUIERDA
+		if(this.entorno.estaPresionada(this.entorno.TECLA_IZQUIERDA) && this.tanque.getAnguloOrientacion()==Math.PI){
+			this.tanque.avanzarIzquierda();
+		}
+		if(this.entorno.estaPresionada(this.entorno.TECLA_IZQUIERDA) && this.tanque.getAnguloOrientacion()!=Math.PI){
+			this.tanque.girar_Izquierda();
+		}
+		//ABAJO
+		if(this.entorno.estaPresionada(this.entorno.TECLA_ABAJO) && this.tanque.getAnguloOrientacion()==Math.PI/2){
+			this.tanque.avanzarAbajo();
+		}
+		if(this.entorno.estaPresionada(this.entorno.TECLA_ABAJO) && this.tanque.getAnguloOrientacion()!=Math.PI/2){
+			this.tanque.girar_Abajo();
+		}
 		
     	this.tanque.dibujar(this.entorno);
 		
